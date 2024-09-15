532215a461ab0532b185df0c532e03bf99fa4122
Hasan Basri <hasannolur06@gmail.com>
2024-09-16 02:36:32 +0300

    deploy için layout alanı kaldırıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css'
 import Light from './Light'
-// import Layout from './layout'
 
 function App() {
   return (
     <Light>
-      {/* <Layout></Layout> */}
       <h1>Hello World</h1>
     </Light>
   )
