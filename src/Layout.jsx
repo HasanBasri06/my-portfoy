485d9332d94b6bb587d69564fc6044ca7438359f
Hasan Basri <hasannolur06@gmail.com>
2024-09-16 01:27:23 +0300

    sosyal medyalar ikonları eklendi

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -15,16 +15,25 @@ function Layout() {
             <div className='myTitle'>
               <div className='myName'>
                 <h1>Hasan Basri Akcıl</h1>
-                <h2>Software Developer</h2>
+                <h2>Full-Stack Developer</h2>
               </div>
               <p>
-              Merhaba
-              <br /><br />
-              to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset shee
+                Merhaba
+                <br /><br />
+                to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset shee
               </p>
             </div>
           </div>
+          <div className='socialMedias'>
+            <div className='linkedin'>
+              <img src='/src/assets/linkedin.svg' />
+            </div>
+            <div className='github'>
+              <img src='/src/assets/github.svg' />
+            </div>
+          </div>
       </div>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       <Footer />
   </div>
   )
